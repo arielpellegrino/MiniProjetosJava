239d626d58662d6c8810324a268ce05573b8ad35
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2022-03-15 09:15:51 -0300

    subindo controle remoto

---
+++ src/ControleRemoto77/main.java
@@ -0,0 +1,12 @@
+package ControleRemoto77;
+
+public class main {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        c.ligarMudo();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
